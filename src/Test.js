e5c02a45f962fac0875d9052a2f000814f593ec4
MichaelWhitt <87039816+MichaelWhitt@users.noreply.github.com>
2023-05-09 12:55:24 -0500

    Squash test (#4)
    * 15
    
    * 16

--- src/Test.js
+++ src/Test.js
@@ -1,7 +1,7 @@
 const Test = () => {
   return (
     <div style={{fontSize: 60, fontWeight: 1000, color: '#909'}}>
-      Base 1 2 3 4 5 6 7 8 9 10 11 12 13 14
+      Base 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
     </div>
   )
 }
